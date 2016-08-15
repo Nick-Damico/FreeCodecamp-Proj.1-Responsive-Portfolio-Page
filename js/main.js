d3c2a6cb01cb1492e8c85e3aaeda457248499eb3
Nicholas D'Amico <nickalan82@icloud.com>
2016-08-14 21:25:39 -0400

    javascript, jQuery additons
    -Add jQuery Plugins
    -Add JavaScript app.js and updated location
    -removed manifest.appcache

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".animsition").animsition({
+  inClass : 'fade-in-right-sm',
+  outClass : 'fade-out-right-sm',
+  linkElement: 'a:not([target="_blank"]):not([href^="#"])',
+  inDuration: 1000,
+  outDuration: 700,
+  browser: [ 'animation-duration', '-webkit-animation-duration']
+});
